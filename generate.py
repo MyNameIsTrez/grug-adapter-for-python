46d9693cca107d11006d6d46a838ed58a2aca709
MyNameIsTrez <welfje@gmail.com>
2025-03-16 21:20:28 +0100

    grug removed the concept of a define() function

--- generate.py
+++ generate.py
@@ -22,9 +22,6 @@ def get_output(mod_api):
 
     output += "\n"
 
-    for entity in mod_api["entities"].keys():
-        output += f"static PyObject *game_fn_define_{entity}_handle;\n"
-
     for fn in mod_api["game_functions"].keys():
         output += f"static PyObject *game_fn_{fn}_handle;\n"
 
@@ -56,12 +53,6 @@ def get_output(mod_api):
 
     output += "\n"
 
-    for entity in mod_api["entities"].keys():
-        output += f'    game_fn_define_{entity}_handle = PyObject_GetAttrString(main_module, "game_fn_define_{entity}");\n'
-        output += f"    CHECK_PYTHON_ERROR();\n"
-        output += f"    assert(game_fn_define_{entity}_handle);\n"
-        output += "\n"
-
     for i, fn in enumerate(mod_api["game_functions"].keys()):
         output += f'    game_fn_{fn}_handle = PyObject_GetAttrString(main_module, "game_fn_{fn}");\n'
         output += f"    CHECK_PYTHON_ERROR();\n"
@@ -73,70 +64,6 @@ def get_output(mod_api):
     output += "}\n"
 
     output += "\n"
-
-    for name, entity in mod_api["entities"].items():
-        output += f"void game_fn_define_{name}("
-
-        for i, field in enumerate(entity["fields"]):
-            if i > 0:
-                output += ", "
-
-            output += field["type"]
-            output += " "
-            output += field["name"]
-
-        output += ") {\n"
-
-        arg_count = len(entity["fields"])
-
-        if arg_count > 0:
-            for field_index, field in enumerate(entity["fields"]):
-                output += f"    PyObject *arg{field_index + 1} = "
-
-                typ = field["type"]
-
-                # TODO: Test all these!
-                if typ == "bool" or typ == "i32":
-                    output += "PyLong_FromLong"
-                elif typ == "id":
-                    output += "PyLong_FromUnsignedLongLong"
-                elif typ == "f32":
-                    output += "PyFloat_FromDouble"
-                elif typ == "string" or typ == "resource" or typ == "entity":
-                    output += "PyUnicode_FromString"
-
-                output += "("
-                output += field["name"]
-                output += ");\n"
-
-                output += "    CHECK_PYTHON_ERROR();\n"
-                output += f"    assert(arg{field_index + 1});\n"
-
-            output += "\n"
-
-            output += f"    PyObject *args = PyTuple_Pack({arg_count}"
-
-            for field_index in range(arg_count):
-                output += f", arg{field_index + 1}"
-
-            output += ");\n"
-
-            output += "    CHECK_PYTHON_ERROR();\n"
-            output += "    assert(args);\n"
-
-            output += "\n"
-
-        output += (
-            f"    PyObject *result = PyObject_CallObject(game_fn_define_{name}_handle, "
-        )
-        output += "args" if arg_count > 0 else "NULL"
-        output += ");\n"
-        output += "    CHECK_PYTHON_ERROR();\n"
-        output += "    assert(result);\n"
-
-        output += "}\n"
-
-        output += "\n"
 
     for i, (name, fn) in enumerate(mod_api["game_functions"].items()):
         output += fn.get("return_type", "void")
